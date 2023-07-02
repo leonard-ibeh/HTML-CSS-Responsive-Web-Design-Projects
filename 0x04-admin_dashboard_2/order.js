81468fde35cab09f015fbf33c1bfc52591d383a3
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-07-02 17:48:19 +0100

    Refactor Codebase To Enhance Project Functionality And Performance

---
+++ 0x04-admin_dashboard_2/order.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: "Dell",
+    productNumber: "85743",
+    paymentStatus: "Due",
+    status: "pending",
+  },
+  {
+    productName: "Samsung",
+    productNumber: "85743",
+    paymentStatus: "Refunded",
+    status: "Declined",
+  },
+  {
+    productName: "Huawei",
+    productNumber: "85743",
+    paymentStatus: "Paid",
+    status: "Active",
+  },
+];
